66ffabbfd9d0721b6c28c0ab5e2a2ec2dd4e9f15
Abel Gómez <abgolla@gmail.com>
2018-05-08 17:20:41 +0200

    Fix feature name in installation script

--- plugins/install-eclipse-oxygen-x86_64.sh
+++ plugins/install-eclipse-oxygen-x86_64.sh
@@ -3,4 +3,4 @@
 source "${0%/*}/_install-eclipse-linux.sh" \
   -u "http://archive.eclipse.org/eclipse/downloads/drops4/R-4.7.3a-201803300640/eclipse-SDK-4.7.3a-linux-gtk-x86_64.tar.gz" \
   -v "oxygen" \
-  -f org.apache.hadoop.hbase.feature.feature.group,io.github.abelgomez.klyo.feature.feature.group,org.eclipse.emf.sdk.feature.group
+  -f org.apache.hadoop.hbase.feature.feature.group,edu.uoc.som.temf.feature.feature.group,org.eclipse.emf.sdk.feature.group
